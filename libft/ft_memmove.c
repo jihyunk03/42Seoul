63652490c721b21fa93f78168de18145ba340e52
Jihyun Kim <ksk208105@gmail.com>
2022-12-30 14:34:45 +0900

    Modify memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 12:28:10 by jihykim2          #+#    #+#             */
-/*   Updated: 2022/11/27 15:04:10 by jihykim2         ###   ########.fr       */
+/*   Updated: 2022/12/30 14:18:06 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,17 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t			i;
-	unsigned char	*d;
-	unsigned char	*s;
+	unsigned char		*d;
+	const unsigned char	*s;
 
 	d = (unsigned char *)dst;
-	s = (unsigned char *)src;
+	s = (const unsigned char *)src;
 	if (dst > src && dst < src + len)
 	{
-		i = len;
-		while (--i >= 0)
+		while (--len >= 0)
 		{
-			d[i] = s[i];
-			if (i == 0)
+			d[len] = s[len];
+			if (len == 0)
 				break ;
 		}
 		return (d);
