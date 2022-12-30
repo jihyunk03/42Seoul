3be546dc882146f30b5a3889e9fa427c1b62d42d
Jihyun Kim <ksk208105@gmail.com>
2022-12-30 12:02:30 +0900

    Modify bzero

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 12:27:27 by jihykim2          #+#    #+#             */
-/*   Updated: 2022/12/11 17:11:59 by jihykim2         ###   ########.fr       */
+/*   Updated: 2022/12/30 12:01:25 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*res;
 	size_t			i;
+	unsigned char	*res;
 
 	i = 0;
 	res = (unsigned char *)s;
