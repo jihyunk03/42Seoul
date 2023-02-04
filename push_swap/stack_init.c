2dd2314a7d7080c90dfb43675e50fe53aca9074d
Jihyun Kim <ksk208105@gmail.com>
2023-02-04 22:40:56 +0900

    Add stack reset funcs

--- push_swap/stack_init.c
+++ push_swap/stack_init.c
@@ -6,30 +6,50 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 16:29:33 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/03 20:50:40 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/04 22:20:23 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*new_stack(char **data)
+t_stack	*stack_init(int len, char **data)
 {
 	t_stack	*new;
+	t_dll	*lst;
 	int		i;
+
+	new = new_stack();
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (++i < len)
+	{
+		lst = new_dll(ft_atoi(data[len]));
+		if (new->head == NULL)
+		{
+			new->head = lst;
+			new->tail = lst;
+		}
+		else
+		{
+			new->tail->next = lst;
+			lst->prev = new->tail;
+		}
+		new->tail = new->tail->next;
+	}
+	return (new);
+}
+
+t_stack	*new_stack(void)
+{
+	t_stack	*new;
 
 	new = malloc(sizeof(t_stack));
 	if (new == NULL)
 		return (NULL);
-	if (data == NULL)
-	{
-		new->head = NULL;
-		new->end = NULL;
-		return (new);
-	}
-	while (*data)
-	{
-		i = ft_atoi(*data);
-	}
+	new->head = NULL;
+	new->tail = NULL;
+	return (new);
 }
 
 t_dll	*new_dll(int data)
@@ -38,10 +58,15 @@ t_dll	*new_dll(int data)
 
 	new = malloc(sizeof(t_dll));
 	if (new == NULL)
-		exit (1);
+		exit (1);	// exit(error) or return (NULL)
 	new->data = data;
 	new->prev = NULL;
 	new->next = NULL;
 	return (new);
 }
 
+t_dll	*dll_add_back(t_dll **end, t_dll *lst)
+{
+	(*end)->next = lst;
+	lst->prev = *end;
+}
