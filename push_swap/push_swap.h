977703bbcc49f22022e04b1fa3e7f1c6c99463bc
Jihyun Kim <ksk208105@gmail.com>
2023-02-04 22:22:05 +0900

    Modify struct's info name

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:05:43 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/01/30 17:07:37 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/04 19:41:25 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 
 # include "./libft/libft.h"
 # include "struct_info.h"
+
+t_stack	*stack_init(int len, char **data);
+t_stack	*new_stack(void);
+t_dll	*new_dll(int data);
 
 #endif
