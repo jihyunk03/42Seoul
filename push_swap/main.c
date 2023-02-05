1d166284905c1beff3ba0f4f38eb553fbc57fdcc
Jihyun Kim <ksk208105@gmail.com>
2023-02-05 23:17:23 +0900

    Add func for check memory leaks

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:12:09 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/05 16:25:03 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/05 23:16:22 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,19 @@ int	main(int ac, char **av)
 	t_stack	*b_stack;
 	char	**arr;	// result of split av
 
+	atexit(check_leak);		// check memory leaks
+	if (ac == 1)
+		exit (0);
+	arr = split_av(ac, av);
 	a_stack = stack_init(ac, av);
 	b_stack = new_stack();
 	if (a_stack == NULL || b_stack == NULL)
 		exit (1);
+}
+
+// 1 2 3 4 "1 2 3 4" 5 6 7 8 9
+// memory leaks
+void check_leak(void)
+{
+     system("leaks a.out");
 }
