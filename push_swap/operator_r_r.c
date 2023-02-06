c964d471bfd72c3de64a38782c32e49410c26b97
Jihyun Kim <ksk208105@gmail.com>
2023-02-06 13:09:16 +0900

    Modify rra, rrb for visualizing

--- push_swap/operator_r_r.c
+++ push_swap/operator_r_r.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 12:23:13 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/06 12:50:26 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/06 13:08:01 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 void	rra(t_stack *a_stack)
 {
-	a_stack->tail->prev->next = NULL;
 	a_stack->tail->next = a_stack->head;
 	a_stack->head->prev = a_stack->tail;
+	a_stack->head = a_stack->tail;
 	a_stack->tail = a_stack->tail->prev;
-	a_stack->head = a_stack->head->prev;
 	a_stack->head->prev = NULL;
+	a_stack->tail->next = NULL;
 }
 
 void	rrb(t_stack *b_stack)
 {
-	b_stack->tail->prev->next = NULL;
 	b_stack->tail->next = b_stack->head;
 	b_stack->head->prev = b_stack->tail;
+	b_stack->head = b_stack->tail;
 	b_stack->tail = b_stack->tail->prev;
-	b_stack->head = b_stack->head->prev;
 	b_stack->head->prev = NULL;
+	b_stack->tail->next = NULL;
 }
 
 void	rrr(t_stack *a_stack, t_stack *b_stack)
