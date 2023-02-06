e27b72611dfa1b61da17fa84c5cc040620507e39
Jihyun Kim <ksk208105@gmail.com>
2023-02-06 21:26:20 +0900

    Add funcs for check argvs & structs

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,31 +6,52 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:12:09 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/05 23:16:22 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/06 20:26:38 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
 	t_stack	*a_stack;
 	t_stack	*b_stack;
-	char	**arr;	// result of split av
 
 	atexit(check_leak);		// check memory leaks
 	if (ac == 1)
 		exit (0);
-	arr = split_av(ac, av);
-	a_stack = stack_init(ac, av);
+	a_stack = stack_init(av);
 	b_stack = new_stack();
 	if (a_stack == NULL || b_stack == NULL)
-		exit (1);
+	{
+		write(1, "Error\n", 6);
+		free_stack(a_stack);
+		free_stack(b_stack);
+		return (0);
+	}
+
+	// test for stack input
+	t_dll	*node = a_stack->tail;
+	int		i = 0;
+	while (av[i])
+		printf("%s\n", av[i++]);
+	printf("\n");
+	while (node)
+	{
+		printf("%d\n", node->data);
+		node = node->prev;
+	}
+
+	// for memory leaks: atexit
+	free_stack(a_stack);
+	free_stack(b_stack);
+	return (0);
 }
 
 // 1 2 3 4 "1 2 3 4" 5 6 7 8 9
 // memory leaks
 void check_leak(void)
 {
-     system("leaks a.out");
+    system("leaks a.out");
 }
