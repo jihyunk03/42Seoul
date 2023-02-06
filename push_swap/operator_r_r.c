37bf8147138fcce37d03e0b194bd0724787e2b14
Jihyun Kim <ksk208105@gmail.com>
2023-02-06 12:52:19 +0900

    Add funcs for rra, rrb, rrr

---
+++ push_swap/operator_r_r.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operator_r_r.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 12:23:13 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/02/06 12:50:26 by jihykim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rra(t_stack *a_stack)
+{
+	a_stack->tail->prev->next = NULL;
+	a_stack->tail->next = a_stack->head;
+	a_stack->head->prev = a_stack->tail;
+	a_stack->tail = a_stack->tail->prev;
+	a_stack->head = a_stack->head->prev;
+	a_stack->head->prev = NULL;
+}
+
+void	rrb(t_stack *b_stack)
+{
+	b_stack->tail->prev->next = NULL;
+	b_stack->tail->next = b_stack->head;
+	b_stack->head->prev = b_stack->tail;
+	b_stack->tail = b_stack->tail->prev;
+	b_stack->head = b_stack->head->prev;
+	b_stack->head->prev = NULL;
+}
+
+void	rrr(t_stack *a_stack, t_stack *b_stack)
+{
+	rra(a_stack);
+	rrb(b_stack);
+}
