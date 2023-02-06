06687b517e579ab0394904fce3c431b2c6f3c1a5
Jihyun Kim <ksk208105@gmail.com>
2023-02-06 21:26:57 +0900

    Add funcs prototypes for gccw

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:05:43 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/06 13:31:12 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/06 21:20:25 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,28 @@
 # include "./libft/libft.h"
 # include "struct_info.h"
 
-// split.c
+# define INT_MAX 2147483647
+# define INT_MIN -2147483648
 
 // stack_init.c
-t_stack	*stack_init(int len, char **data);
+t_stack	*stack_init(char **data);
 t_stack	*new_stack(void);
-t_dll	*new_dll(int data);
+t_dll	*new_dll(int data, int flag);
+
+// stack_init_utils.c
+int		push_to_stack(t_stack *new, char *data);
+int		is_integer(char *s);
+int		push_swap_atoi(const char *str, int *flag);
 void	dll_add_back(t_stack *new, t_dll *lst);
 
 // free_all.c
 void	*free_stack(t_stack *stack);
-void	*free_arr(char **arr);
+int		free_arr(char **arr);
+
+// libft prototypes for test & check leaks => must remove!!
+void 	check_leak(void);
+int		ft_isdigit(int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	**ft_split(char const *s, char c);
 
 #endif
