9acc6fe55a2af313130eb37c4136c31ef0bde6f5
Jihyun Kim <ksk208105@gmail.com>
2023-02-06 21:24:53 +0900

    Add funcs for check overflow and split values(no invalid input)

--- push_swap/stack_init.c
+++ push_swap/stack_init.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 16:29:33 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/06 18:07:20 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/06 21:22:30 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 t_stack	*stack_init(char **data)
 {
 	t_stack	*new;
-	t_dll	*lst;
 	int		i;
 
 	new = new_stack();
@@ -25,7 +24,7 @@ t_stack	*stack_init(char **data)
 	while (data[++i])
 	{
 		if (push_to_stack(new, data[i]) == 0)
-			return (NULL);
+			return (free_stack(new));
 	}
 	return (new);
 }
@@ -43,10 +42,12 @@ t_stack	*new_stack(void)
 	return (new);
 }
 
-t_dll	*new_dll(int data)
+t_dll	*new_dll(int data, int flag)
 {
 	t_dll	*new;
 
+	if (flag)
+		return (NULL);
 	new = malloc(sizeof(t_dll));
 	if (new == NULL)
 		return (NULL);
@@ -55,41 +56,3 @@ t_dll	*new_dll(int data)
 	new->next = NULL;
 	return (new);
 }
-
-int	push_to_stack(t_stack *new, char *data)
-{
-	t_dll	*lst;
-	int		i;
-	char	**arr;
-
-	arr = ft_split(data, ' ');
-	if (arr == NULL)
-		return (0);
-	i = 0;
-	while (arr[i])
-	{
-		lst = new_dll(ft_atoi(arr[i++]));
-		if (lst == NULL)
-			return (free_arr(arr));
-		dll_add_back(new, lst);
-	}
-	free_arr(arr);
-	return (1)
-}
-
-void	dll_add_back(t_stack *new, t_dll *lst)
-{
-	if (new->head == NULL)
-	{
-		new->head = lst;
-		new->tail = lst;
-	}
-	else
-	{
-		new->tail->next = lst;
-		lst->prev = new->tail;
-		new->tail = lst;
-	}
-	new->size++;
-}
-
--- push_swap/stack_init_utils.c
+++ push_swap/stack_init_utils.c
@@ -1,25 +1,91 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   split.c                                            :+:      :+:    :+:   */
+/*   stack_init_utils.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 20:59:58 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/06 17:04:54 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/06 21:22:29 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-char	**split_av(char **av)
+int	push_to_stack(t_stack *new, char *data)
 {
+	t_dll	*lst;
 	int		i;
-	int		len;
+	int		flag;
 	char	**arr;
 
-	i = 1;
-	while (av[i])
+	arr = ft_split(data, ' ');
+	if (arr == NULL)
+		return (0);
+	i = 0;
+	flag = 0;
+	while (arr[i])
 	{
+		if (is_integer(arr[i]) == 0)
+			return (free_arr(arr));
+		lst = new_dll(push_swap_atoi(arr[i++], &flag), flag);
+		if (lst == NULL || flag)
+			return (free_arr(arr));
+		dll_add_back(new, lst);
 	}
+	free_arr(arr);
+	return (1);
+}
+
+int	is_integer(char *s)
+{
+	if (*s == '+' || *s == '-')
+		s++;
+	while (*s)
+	{
+		if (ft_isdigit(*s++) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+int	push_swap_atoi(const char *str, int *flag)
+{
+	long	res;
+	int		len;
+	int		sign;
+
+	res = 0;
+	len = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str++ == '-')
+			sign *= -1;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str++ - '0');
+		if (++len > 10 || res < INT_MIN || res > INT_MAX)
+			(*flag)++;
+	}
+	return (sign * res);
+}
+
+void	dll_add_back(t_stack *new, t_dll *lst)
+{
+	if (new->head == NULL)
+	{
+		new->head = lst;
+		new->tail = lst;
+	}
+	else
+	{
+		new->tail->next = lst;
+		lst->prev = new->tail;
+		new->tail = lst;
+	}
+	new->size++;
 }
