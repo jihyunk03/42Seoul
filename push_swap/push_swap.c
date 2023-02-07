8a6d9d9594c115ecd7ffc8309a7f99cf0499c2a6
Jihyun Kim <ksk208105@gmail.com>
2023-02-08 01:19:46 +0900

    Renamed: main.c -> push_swap.c

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/30 17:12:09 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/07 01:15:08 by jihykim2         ###   ########.fr       */
+/*   Created: 2023/02/07 13:23:41 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/02/08 01:18:14 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,21 @@ int	main(int ac, char **av)
 	t_stack	*a_stack;
 	t_stack	*b_stack;
 
-	atexit(check_leak);		// check memory leaks
+	// check memory leaks
+	atexit(check_leak);
 	if (ac == 1)
 		exit (0);
+	// stack init
 	a_stack = stack_init(av);
 	b_stack = new_stack();
-	if (a_stack == NULL || b_stack == NULL)	// 더 수정할 것(error 처리)
+	if (a_stack == NULL || b_stack == NULL)
 	{
 		write(1, "Error\n", 6);
-		free_stack(a_stack);
-		free_stack(b_stack);
-		return (0);
+		exit (0);
 	}
 
-	// test for stack input
-	t_dll	*node = a_stack->head;
-	int		i = 0;
-	while (av[i])
-		printf("%s\n", av[i++]);
-	printf("\n");
-	while (node)
-	{
-		printf("%d\n", node->idx);
-		node = node->next;
-	}
+	// push_swap
+
 
 	// for memory leaks: atexit
 	free_stack(a_stack);
@@ -51,7 +42,23 @@ int	main(int ac, char **av)
 
 // 1 2 3 4 "1 2 3 4" 5 6 7 8 9
 // memory leaks
-void check_leak(void)
+void	check_leak(void)
 {
     system("leaks a.out");
+}
+
+void	check_operator_result(t_stack *stack, char *op)
+{
+	t_dll	*node;
+	int		i;
+
+	node = stack->head;
+	i = 0;
+	printf("=====name: %s=====\n", op);
+	while (node)
+	{
+		printf("idx[%d]: %d\n", i++, node->idx);
+		node = node->next;
+	}
+	printf("=====size: %d=====\n\n", stack->size);
 }
