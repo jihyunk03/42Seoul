0d07a90d6e390f9ca6cd457c184b7ac50ec068cb
Jihyun Kim <ksk208105@gmail.com>
2023-02-07 14:57:14 +0900

    Modify for bugs: seg fault

--- push_swap/operator_p.c
+++ push_swap/operator_p.c
@@ -6,11 +6,13 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 12:23:07 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/06 12:34:56 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/07 14:37:46 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	p_to_stack(t_stack *stack, t_dll *lst);
 
 void	pa(t_stack *from, t_stack *to)
 {
@@ -18,13 +20,12 @@ void	pa(t_stack *from, t_stack *to)
 
 	if (from->head == NULL)
 		return ;
+
 	tmp = from->head;
 	from->head = tmp->next;
 	from->head->prev = NULL;
-	to->head->prev = tmp;
-	tmp->next = to->head;
-	tmp->prev = NULL;
-	to->head = tmp;
+	p_to_stack(to, tmp);
+	from->size--;
 }
 
 void	pb(t_stack *from, t_stack *to)
@@ -33,11 +34,30 @@ void	pb(t_stack *from, t_stack *to)
 
 	if (from->head == NULL)
 		return ;
+
 	tmp = from->head;
 	from->head = tmp->next;
 	from->head->prev = NULL;
-	to->head->prev = tmp;
-	tmp->next = to->head;
-	tmp->prev = NULL;
-	to->head = tmp;
+	p_to_stack(to, tmp);
+	from->size--;
+}
+
+static void	p_to_stack(t_stack *stack, t_dll *lst)
+{
+	if (stack->head == NULL)
+	{
+		stack->head = lst;
+		stack->tail = lst;
+		lst->prev = NULL;
+		lst->next = NULL;
+	}
+	else
+	{
+		stack->head->prev = lst;
+		lst->next = stack->head;
+		lst->prev = NULL;
+		stack->head = lst;
+	}
+	lst->prev = NULL;
+	stack->size++;
 }
