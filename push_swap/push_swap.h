0ade64812c2a84c100dfb4dfff67d8951d55b951
Jihyun Kim <ksk208105@gmail.com>
2023-02-08 11:50:45 +0900

    Add prototypes

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,15 +6,16 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:05:43 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/07 01:14:33 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/08 00:57:27 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP
-# define PUSH_SWAP
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
 # include "./libft/libft.h"
 # include "struct_info.h"
+# include "error_define.h"
 
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
@@ -36,8 +37,28 @@ void	*free_stack(t_stack *stack);
 int		free_dll(t_dll *lst);
 int		free_arr(char **arr);
 
+// operator.c
+void	sa(t_stack *a_stack);
+void	sb(t_stack *b_stack);
+void	ss(t_stack *a_stack, t_stack *b_stack);
+void	pa(t_stack *from, t_stack *to);
+void	pb(t_stack *from, t_stack *to);
+void	ra(t_stack *a_stack);
+void	rb(t_stack *b_stack);
+void	rr(t_stack *a_stack, t_stack *b_stack);
+void	rra(t_stack *a_stack);
+void	rrb(t_stack *b_stack);
+void	rrr(t_stack *a_stack, t_stack *b_stack);
+
+// push_swap.c
+void	sort_to_b(t_stack * a_stack, t_stack *b_stack);
+void	sort_to_a(t_stack * a_stack, t_stack *b_stack);
+void	a_to_b(t_stack *a_stack, t_stack *b_stack, int num, int chunk);
+
+
 // libft prototypes for test & check leaks => must remove!!
 void 	check_leak(void);
+void	check_operator_result(t_stack *stack, char *op);
 int		ft_isdigit(int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	**ft_split(char const *s, char c);
