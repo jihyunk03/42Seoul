2c9344e51362b9dadfd0656ff6f823cf6440db09
Jihyun Kim <ksk208105@gmail.com>
2023-02-11 18:43:22 +0900

    Modify bugs for enum ERROR

--- push_swap/number_define.h
+++ push_swap/number_define.h
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_define.h                                     :+:      :+:    :+:   */
+/*   number_define.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:22:21 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/07 15:38:01 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/11 18:33:54 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ERROR_DEFINE_H
-# define ERROR_DEFINE_H
+#ifndef NUMBER_DEFINE_H
+# define NUMBER_DEFINE_H
 
-typedef enum Error
+# define INT_MAX 2147483647
+# define INT_MIN -2147483648
+
+enum e_error
 {
 	NoInt = 0,
 	Overflow,
