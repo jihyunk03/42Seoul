182ab33d05d8e7ae468ca9f2eda108f1c87d1bbb
Jihyun Kim <ksk208105@gmail.com>
2023-02-12 23:03:24 +0900

    Modify file name & Add operator defines

--- push_swap/define_enum.h
+++ push_swap/define_enum.h
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:22:21 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/11 18:33:54 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/12 21:41:29 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
+
+# define SA "sa\n"
+# define SB "sb\n"
+# define SS "ss\n"
+# define PA "pa\n"
+# define PB "pb\n"
+# define RA "ra\n"
+# define RB "rb\n"
+# define RR "rr\n"
+# define RRA "rra\n"
+# define RRB "rrb\n"
+# define RRR "rrr\n"
 
 enum e_error
 {
