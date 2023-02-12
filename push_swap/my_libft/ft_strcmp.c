f0865041c0ac4c48eba21c9c4aa229b9f17d0845
Jihyun Kim <ksk208105@gmail.com>
2023-02-13 02:21:48 +0900

    Modify bugs

--- push_swap/my_libft/ft_strcmp.c
+++ push_swap/my_libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 22:32:44 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/12 22:36:06 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/13 02:05:40 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 			break ;
 		i++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s1[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
