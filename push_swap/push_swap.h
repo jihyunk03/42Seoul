282134cbc29ddd612a5fa98ad0a0ce378313a686
Jihyun Kim <ksk208105@gmail.com>
2023-02-12 19:41:23 +0900

    Modify orders

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:05:43 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/12 16:45:35 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/12 18:54:25 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@
 
 # include "./my_libft/libft.h"
 # include "./my_libft/ft_printf.h"
-# include "./my_libft/get_next_line.h"
+// # include "./my_libft/get_next_line.h"
 # include "struct_info.h"
 # include "number_define.h"
+
+// push_swap.c
+void	sort_to_b(t_stack * a_stack, t_stack *b_stack);
+void	sort_to_a(t_stack * a_stack, t_stack *b_stack);
+void	a_to_b(t_stack *a_stack, t_stack *b_stack, int num, int chunk);
 
 // stack_init.c
 t_stack	*stack_init(char **data);
@@ -36,24 +41,6 @@ void	*free_stack(t_stack *stack);
 int		free_dll(t_dll *lst);
 int		free_arr(char **arr);
 
-// operator.c
-void	sa(t_stack *a_stack);
-void	sb(t_stack *b_stack);
-void	ss(t_stack *a_stack, t_stack *b_stack);
-void	pa(t_stack *from, t_stack *to);
-void	pb(t_stack *from, t_stack *to);
-void	ra(t_stack *a_stack);
-void	rb(t_stack *b_stack);
-void	rr(t_stack *a_stack, t_stack *b_stack);
-void	rra(t_stack *a_stack);
-void	rrb(t_stack *b_stack);
-void	rrr(t_stack *a_stack, t_stack *b_stack);
-
-// push_swap.c
-void	sort_to_b(t_stack * a_stack, t_stack *b_stack);
-void	sort_to_a(t_stack * a_stack, t_stack *b_stack);
-void	a_to_b(t_stack *a_stack, t_stack *b_stack, int num, int chunk);
-
 // check_sort.c
 int		check_sort(t_stack *stack, int size);
 
@@ -68,6 +55,19 @@ int		is_in_stack(t_stack *stack, int num);
 void	sort_to_a(t_stack *a_stack, t_stack *b_stack);
 int		find_value(t_stack *stack, int value);
 void	rotate_for_push(t_stack *stack, int cnt);
+
+// operator.c
+void	sa(t_stack *a_stack);
+void	sb(t_stack *b_stack);
+void	ss(t_stack *a_stack, t_stack *b_stack);
+void	pa(t_stack *from, t_stack *to);
+void	pb(t_stack *from, t_stack *to);
+void	ra(t_stack *a_stack);
+void	rb(t_stack *b_stack);
+void	rr(t_stack *a_stack, t_stack *b_stack);
+void	rra(t_stack *a_stack);
+void	rrb(t_stack *b_stack);
+void	rrr(t_stack *a_stack, t_stack *b_stack);
 
 // libft prototypes for test & check leaks => must remove!!
 void 	check_leak(void);
