db8db284b281b147506e24ec9d687443e40aa1a6
Jihyun Kim <ksk208105@gmail.com>
2023-02-20 22:02:24 +0900

    Add fdf files

--- fdf/fdf_files/fdf.h
+++ fdf/fdf_files/fdf.h
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   struct_info.h                                      :+:      :+:    :+:   */
+/*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/18 17:10:58 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/20 15:05:18 by jihykim2         ###   ########.fr       */
+/*   Created: 2023/02/20 15:03:53 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/02/20 21:24:17 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRUCT_INFO_H
-# define STRUCT_INFO_H
+#ifndef FDF_H
+# define FDF_H
 
-typedef struct s_map
-{
-	int	depth;
-}	t_map;
-
+# include "../my_libft/libft.h"
+# include "../my_libft/ft_printf.h"
+# include "../my_libft/get_next_line.h"
+# include "struct_info.h"
+# include <fcntl.h>
 
 #endif
--- fdf/fdf_files/map_parse.c
+++ fdf/fdf_files/map_parse.c
@@ -1,22 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   struct_info.h                                      :+:      :+:    :+:   */
+/*   map_parse.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/18 17:10:58 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/20 15:05:18 by jihykim2         ###   ########.fr       */
+/*   Created: 2023/02/20 14:16:39 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/02/20 15:05:31 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRUCT_INFO_H
-# define STRUCT_INFO_H
-
-typedef struct s_map
-{
-	int	depth;
-}	t_map;
-
-
-#endif
+#include "fdf.h"
