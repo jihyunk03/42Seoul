64de766e0cb7b238645d8f7d69f299cf8c21018f
Jihyun Kim <ksk208105@gmail.com>
2023-02-20 15:02:51 +0900

    Add my_libft folder

---
+++ fdf/my_libft/ft_memcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihykim2 <jihykim2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 12:27:47 by jihykim2          #+#    #+#             */
+/*   Updated: 2022/11/27 15:20:13 by jihykim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	i = 0;
+	d = (unsigned char *)dst;
+	s = (const unsigned char *)src;
+	if (d != s)
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (d);
+}
