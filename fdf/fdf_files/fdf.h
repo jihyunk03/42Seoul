6b644103eb026ebf3fca82a81c755f7c05690813
Jihyun Kim <ksk208105@gmail.com>
2023-02-26 23:45:56 +0900

    Modify: header file

--- fdf/fdf_files/fdf.h
+++ fdf/fdf_files/fdf.h
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 15:03:53 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/20 21:24:17 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/23 15:49:35 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "../my_libft/libft.h"
 # include "../my_libft/ft_printf.h"
 # include "../my_libft/get_next_line.h"
+# include "../mlx/mlx.h"
 # include "struct_info.h"
 # include <fcntl.h>
 
