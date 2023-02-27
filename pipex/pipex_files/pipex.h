9c3d7dbcd12b0164627814dc83ff1c5c3035a534
Jihyun Kim <ksk208105@gmail.com>
2023-02-28 04:14:42 +0900

    Modify: header file

--- pipex/pipex_files/pipex.h
+++ pipex/pipex_files/pipex.h
@@ -6,12 +6,17 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:52:12 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/23 17:44:37 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/02/27 17:07:47 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+# include "../my_libft/libft.h"
+# include "../my_libft/ft_printf.h"
+# include "../my_libft/get_next_line.h"
+# include <fcntl.h>
 
 typedef struct s_args
 {
