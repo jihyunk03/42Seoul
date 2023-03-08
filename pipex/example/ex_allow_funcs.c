30ab487658873e5e50efe8557387f5b6cd2dda92
Jihyun Kim <ksk208105@gmail.com>
2023-03-08 17:00:29 +0900

    Modify: allow_funcs

--- pipex/example/ex_allow_funcs.c
+++ pipex/example/ex_allow_funcs.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 09:55:10 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/03/07 14:17:13 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/03/08 16:45:32 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,20 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+
+#include <sys/types.h>
+
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+int	main(void)
+{
+	char	write_msg[BUFFER_SIZE] = "Greetings";
+	char	read_msg[BUFFER_SIZE];
+	int		fd[2];
+	pid_t	pid;
+
+	//파이프 생성
+	if (pipe(fd) == -1)
+}
