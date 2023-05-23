8ff6c4c4b533a7cc680a57db5f0f79a5f72e5537
Jihyun Kim <ksk208105@gmail.com>
2023-05-23 20:14:24 +0900

    Modify: no_wait(for test)

--- pipex/example/wait/no_wait.c
+++ pipex/example/wait/no_wait.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 15:50:32 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/05/23 17:32:46 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/05/23 20:10:33 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,23 @@
 
 int	main(void)
 {
+	pid_t	pid;
 
+	pid = fork();
+	if (pid == -1)
+		return (0);
+	else if (pid == 0)
+	{
+		printf("=====This is Child Process=====\n");
+		printf("=====END: Child Process=====\n");
+	}
+	else
+	{
+		printf("-----This is Parent Process-----\n");
+		printf("I have child process(id: %d)\n", pid);
+		while (1)
+			;
+		printf("-----END: Parent Process-----\n");
+	}
 	return (0);
 }
