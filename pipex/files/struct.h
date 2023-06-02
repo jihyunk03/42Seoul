83f04e65296745e0b26162ee3295ec933577b15b
Jihyun Kim <ksk208105@gmail.com>
2023-06-02 21:06:16 +0900

    Modify: header for include & struct

--- pipex/files/struct.h
+++ pipex/files/struct.h
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/23 15:52:12 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/06/02 21:02:40 by jihykim2         ###   ########.fr       */
+/*   Created: 2023/06/02 21:00:50 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/06/02 21:03:58 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef STRUCT_H
+# define STRUCT_H
 
-# include "../my_libft/libft.h"
-# include "../my_libft/ft_printf.h"
-# include "../my_libft/get_next_line.h"
-# include "./struct.h"
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
-
-# define TRUE 1
-# define FALSE 0
+typedef struct s_args
+{
+	int		high;
+	int		length;
+	char	**arr;
+}	t_args;
 
 #endif
