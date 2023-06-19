9ac2ef896fd9abf4912672bc2d30b5a5dc0248f8
Jihyun Kim <ksk208105@gmail.com>
2023-06-19 14:09:18 +0900

    Modify: locations(files) for includes

--- pipex/srcs/argument_check.c
+++ pipex/srcs/argument_check.c
@@ -6,11 +6,11 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 03:47:22 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/03/02 23:29:00 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/06/19 14:06:28 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "../includes/pipex.h"
 
 int	argument_check(char **av)
 {
--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,11 +6,11 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:52:02 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/02/23 17:43:53 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/06/19 14:06:05 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "../includes/pipex.h"
 
 int	main(void)
 {
