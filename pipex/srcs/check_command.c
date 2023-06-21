cf1f902242ba9c4f447b210ff2de8dd5e0cfd980
Jihyun Kim <ksk208105@gmail.com>
2023-06-21 09:15:56 +0900

    Modify: filename

--- pipex/srcs/check_command.c
+++ pipex/srcs/check_command.c
@@ -1,23 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   argument_check.c                                   :+:      :+:    :+:   */
+/*   check_command.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 03:47:22 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/06/19 14:06:28 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/06/21 09:09:48 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int	argument_check(char **av)
+char	**split_command(char *cmd)
 {
 	int	i;
 
 	i = 0;
-	while (av[i])
-	{
-	}
 }
