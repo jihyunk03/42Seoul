b7b100f0e43e78cdd1ae5da44a30e814bac506d4
Jihyun Kim <ksk208105@gmail.com>
2023-06-26 14:08:08 +0900

    Modify: execute_command for bugs(strjoin about '/')

--- pipex/srcs/error_exit.c
+++ pipex/srcs/error_exit.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 04:53:02 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/06/21 05:04:23 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/06/26 12:59:18 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,12 @@ int	error_exit(char *s, int true)
 	}
 	ft_putchar_fd(s, STDERR_FILENO);
 	return (EXIT_FAILURE);
+}
+
+int	error_no_command(char *no_path)
+{
+	ft_putstr_fd("pipex: ", STDERR_FILENO);
+	ft_putstr_fd(no_path, STDERR_FILENO);
+	ft_putstr_fd(": command not found\n", STDERR_FILENO);
+	return (127);
 }
--- pipex/srcs/execute_command.c
+++ pipex/srcs/execute_command.c
@@ -6,23 +6,66 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 20:07:33 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/06/24 15:14:13 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/06/26 12:59:40 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
+static char	*_check_command_path(t_pipe *p, char *cmd);
+
 void	exec_command(t_pipe *p, char **cmd_arr)
 {
 	char	*cmd_path;		// path for exec() first argument
+
+	if (execve(cmd_arr[0], cmd_arr, p->envp) == 0)	// 절대 경로인 경우
+		exit (EXIT_SUCCESS);
+	cmd_path = _check_command_path(p, cmd_arr[0]);
+	if (cmd_path == NULL)
+		exit (error_no_command(cmd_arr[0]));
+	if (execve(cmd_path, cmd_arr, p->envp) == -1)
+	{
+		free (cmd_path);	// cmd_arr도 free?
+		exit (127);		// exit number(127) in terminal
+	}
+	free (cmd_path);
+}
+
+static char	*_check_command_path(t_pipe *p, char *cmd)
+{
+	char	*cmd_path;
+	char	*tmp;
 	int		i;
 
 	i = 0;
-	if (execve(cmd_arr[i], cmd_arr, p->envp) == 0)	// 절대 경로인 경우
-		exit (EXIT_SUCCESS);
 	while (p->path[i])
 	{
-		cmd_path = ft_strjoin(p->path[i], cmd_arr[0]);
+		tmp = ft_strjoin(p->path[i], "/");
+		if (tmp == NULL)
+			exit (EXIT_FAILURE);		// error (1)
+		cmd_path = ft_strjoin(tmp, cmd);
+		free (tmp);
+		if (cmd_path == NULL)
+			exit (EXIT_FAILURE);		// error (1)
+		if (access(cmd_path, X_OK) == 0)
+			return (cmd_path);
+		free (cmd_path);
+		i++;
+	}
+	return (NULL);
+}
+
+/*
+	if (execve(cmd_arr[0], cmd_arr, p->envp) == 0)	// 절대 경로인 경우
+		exit (EXIT_SUCCESS);
+	i = 0;
+	while (p->path[i])
+	{
+		tmp = ft_strjoin(p->path[i], "/");
+		if (tmp == NULL)
+			exit (EXIT_FAILURE);		// message: strjoin error
+		cmd_path = ft_strjoin(tmp, cmd_arr[0]);
+		free (tmp);
 		if (cmd_path == NULL)
 			exit (EXIT_FAILURE);		// message: strjoin error, [유의]자식 프로세스만 종료됨 >> path_arr 모두 free해야 함
 		if (execve(cmd_path, cmd_arr, p->envp) == 0)
@@ -33,5 +76,5 @@ void	exec_command(t_pipe *p, char **cmd_arr)
 		free (cmd_path);
 		i++;
 	}
-	perror(cmd_arr[0]);		// 아마 perror가 안되면 직접 문자열을 적어야 함
-}
+	exit (127);
+*/
