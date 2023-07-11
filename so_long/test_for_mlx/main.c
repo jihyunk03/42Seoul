3a26f4d75862a5d10b6ad26b3b0ee069da10e003
Jihyun Kim <ksk208105@gmail.com>
2023-07-11 18:26:31 +0900

    Modify: for test mlx

--- so_long/test_for_mlx/main.c
+++ so_long/test_for_mlx/main.c
@@ -1,12 +1,52 @@
 #include "./main.h"
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+int		key_hook(int keycode, t_vars *vars);
+void	print_values(t_data *image);
+
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_vars	vars;
+	t_data	image;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "hello world");
-	mlx_loop(mlx_ptr);
+	vars.mlx_ptr = mlx_init();
+	vars.win_ptr = mlx_new_window(vars.mlx_ptr, 500, 500, "TITLE^*^");
+	image.img = mlx_new_image(vars.mlx_ptr, 500, 500);
+	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
+	print_values(&image);
+	for (int i = 0; i < 500; i++)
+		for (int j = 0; j < 500; j++)
+			my_mlx_pixel_put(&image, i, j, 0x0000FF00);
+	mlx_put_image_to_window(vars.mlx_ptr, vars.win_ptr, image.img, 0, 0);
+	mlx_key_hook(vars.win_ptr, key_hook, &vars);
+	mlx_loop(vars.mlx_ptr);
 	return (0);
+}
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	key_hook(int keycode, t_vars *vars)
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_window(vars->mlx_ptr, vars->win_ptr);
+		exit (0);
+	}
+	return (0);
+}
+
+void	print_values(t_data *image)
+{
+	printf("==struct of image==\n");
+	printf("[ptr of image]: %p\n", image->img);
+	printf("[char of addr]: %s\n", image->addr);
+	printf("[bits_per_pixel]: %d\n", image->bits_per_pixel);
+	printf("[line_length]: %d\n", image->line_length);
+	printf("[endian]: %d\n", image->endian);
 }
--- so_long/test_for_mlx/main.h
+++ so_long/test_for_mlx/main.h
@@ -5,4 +5,19 @@
 # include <stdlib.h>
 # include "./mlx/mlx.h"
 
+typedef struct s_vars
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_vars;
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
 #endif
