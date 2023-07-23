8dd18fc567a839124c0fd87c2c7dc3433dfc42bd
Jihyun Kim <ksk208105@gmail.com>
2023-07-24 03:51:16 +0900

    New: [philo] files for start

--- philosophers/includes/header.h
+++ philosophers/includes/header.h
@@ -1,12 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.c                                     :+:      :+:    :+:   */
+/*   header.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/21 22:16:10 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/07/21 22:16:11 by jihykim2         ###   ########.fr       */
+/*   Created: 2023/07/24 03:42:53 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/07/24 03:44:46 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HEADER_H
+# define HEADER_H
+
+# include "../my_libft/libft/libft.h"
+# include "../my_libft/get_next_line/get_next_line.h"
+# include "../my_libft/ft_printf/ft_printf.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
