d78b7d5a5ec7a654d507f255bcc1d597d490c4a9
Jihyun Kim <ksk208105@gmail.com>
2023-07-25 14:38:16 +0900

    New: [locations] for philo and bonus

--- philosophers/philo/includes/header.h
+++ philosophers/philo/includes/header.h
@@ -6,16 +6,13 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 03:42:53 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/07/24 03:44:46 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/07/25 14:37:00 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
 
-# include "../my_libft/libft/libft.h"
-# include "../my_libft/get_next_line/get_next_line.h"
-# include "../my_libft/ft_printf/ft_printf.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
