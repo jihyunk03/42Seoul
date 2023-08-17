5b356447478c75d539e3b9f0e464b9afca76daae
Jihyun Kim <ksk208105@gmail.com>
2023-08-17 21:08:07 +0900

    Rename: [file name]

--- philosophers/philo/srcs/current_time.c
+++ philosophers/philo/srcs/current_time.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_and_time.c                                   :+:      :+:    :+:   */
+/*   current_time.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 18:38:10 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/08/17 20:56:01 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/08/17 21:05:36 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,4 @@ long long	current_time(void)
 
 	gettimeofday(&tv, NULL);
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
-}
-
-void	print_message(t_philo *philo, char *message)
-{
-	pthread_mutex_lock(&philo->data->print);
-	if (philo->data->print_dead_flag == TRUE)
-	{
-		pthread_mutex_unlock(&philo->data->print);
-		return ;
-	}
-	printf("%lld %d %s\n", current_time() - philo->data->start_t, \
-	philo->id, message);
-	pthread_mutex_unlock(&philo->data->print);
-}
-
-void	print_dead(t_philo *philo, t_data *data)
-{
-	pthread_mutex_lock(&data->print);
-	if (data->print_dead_flag == TRUE)
-	{
-		pthread_mutex_unlock(&data->print);
-		return ;
-	}
-	data->print_dead_flag = TRUE;
-	printf("%lld %d %s\n", current_time() - data->start_t, \
-	philo->id, DEAD);
-	pthread_mutex_unlock(&data->print);
 }
