d6c5ec881dc34175c97faab59614dd5ae9d13aaf
Jihyun Kim <ksk208105@gmail.com>
2023-08-17 20:28:44 +0900

    New: [philo_bonus]

---
+++ philosophers/philo_bonus/includes/philo_bonus.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 20:11:26 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/08/17 20:19:33 by jihykim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include "./struct_bonus.h"
+# include "./define_bonus.h"
+# include "./header_bonus.h"
+
+#endif
