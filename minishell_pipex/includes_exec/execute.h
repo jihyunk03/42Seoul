f5a534413182d4b3be4f077f2326fc95229a1dab
Jihyun Kim <ksk208105@gmail.com>
2023-09-04 17:41:40 +0900

    Modify: [includes]

--- minishell_pipex/includes_exec/execute.h
+++ minishell_pipex/includes_exec/execute.h
@@ -6,14 +6,26 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 19:45:22 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/09/02 20:02:56 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/09/04 16:58:45 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTE_H
 # define EXECUTE_H
 
-# include "./include.h"
+# include "../includes/minishell.h"
+# include "./struct.h"
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <dirent.h>
+# include <term.h>
+# include <sys/ioctl.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
 
 /* [ function prototype ] */
 
